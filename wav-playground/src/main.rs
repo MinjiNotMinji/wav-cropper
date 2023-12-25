a320bb490728dae670b9bbe9da4b8fb521c45c4a
Yul Kang <yulkang2019@gmail.com>
2023-12-25 19:56:03 +0900

    Update main.rs
    changed to use 'sink'

--- wav-playground/src/main.rs
+++ wav-playground/src/main.rs
@@ -1,18 +1,15 @@
 use std::fs::File;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source};
+use rodio::{Decoder, OutputStream, source::Source, Sink};
 
 fn main() {
     // Get a output stream handle to the default physical sound device
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
     // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("../data/PinkPanther30.wav").unwrap());
-    // Decode that sound file into a source
+    let file = File::open("../data/PinkPanther30.wav").unwrap();
     let source = Decoder::new(file).unwrap();
-    // Play the sound directly on the device
-    stream_handle.play_raw(source.convert_samples());
 
-    // The sound plays in a separate audio thread,
-    // so we need to keep the main thread alive while it's playing.
-    std::thread::sleep(std::time::Duration::MAX);
+    sink.append(source);
+    sink.sleep_until_end();
 }
